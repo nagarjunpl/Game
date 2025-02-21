1f58e33580358c7905e065a9a64376d0449a7217
Nagarjun PL <nagarjunpl4400@gmail.com>
2025-02-21 18:16:01 +0530

    Update App.tsx

--- Game/src/App.tsx
+++ Game/src/App.tsx
@@ -255,9 +255,7 @@ function App() {
       <div className="max-w-4xl w-full bg-white rounded-2xl shadow-xl p-8">
         <div className="flex items-center justify-center gap-2 mb-8">
           <Brain className="w-8 h-8 text-blue-500" />
-          <br>
-          <h3 > Edit by Nagarjun </h3>
-          </br>
+          <h3 className="text-3xl font-bold text-gray-800"> Edit by Nagarjun </h3>
           <h1 className="text-3xl font-bold text-gray-800"> Math Puzzle </h1>
           
         </div>
